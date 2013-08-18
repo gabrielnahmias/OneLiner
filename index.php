b609665856f445421a39fc89d1a51911b269df36
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-08-18 12:52:29 -0500

    Added loop that adds links to all theme CSS files, added jQuery event assignment to theme selector instead of inline, and changed some script.

--- index.php
+++ index.php
@@ -20,6 +20,11 @@ foreach($dirs as $dir) {
 	if (!in_array($dir, array(".","..","index.html","meta.js","rpm")))
 		print "<script src=\"".DIR_CM."/mode/$dir/$dir.js\"></script>\r\n";
 }
+$files = scandir(DIR_CM."/theme");
+foreach($files as $file) {
+	if (!in_array($file, array(".","..","index.html")))
+		print "<link rel=\"stylesheet\" href=\"".DIR_CM."/theme/$file\" type=\"text/css\">";
+}
 ?>
 <!-- jQuery -->
 <script src="<?=DIR_JS?>/jquery-1.10.2.min.js" type="text/javascript"></script>
@@ -27,7 +32,6 @@ foreach($dirs as $dir) {
 <script src="<?=DIR_JS?>/scripts.js" type="text/javascript"></script>
 <script type="text/javascript">
 var editor;
-
 $(function() {
 	if (!String.prototype.reduce) {
 		String.prototype.reduce = function() {
@@ -62,6 +66,7 @@ $(function() {
 	
 	$("#main").submit(change);
 	$("input[type=submit]").click(change);
+	$("#theme").change(OL.events.selectTheme);
 });
 </script>
 </head>
@@ -70,7 +75,7 @@ $(function() {
     	<header>
             <h1>OneLiner</h1>
             <span>reduce your text</span>
-            <select onchange="selectTheme()" id="theme">
+            <select id="theme">
                 <option value="">Select a theme</option>
 				<?php
                 $files = scandir(DIR_CM."/theme");
@@ -92,15 +97,14 @@ $(function() {
         </form>
     </div>
     <script type="text/javascript">
-	var choice,
+	var choice = document.location.search && decodeURIComponent(document.location.search.slice(1)),
 		pending,
-		input;
+		input = document.getElementById("theme");
 	editor = CodeMirror.fromTextArea($("#input")[0], {
 		continuousScanning: 500,
 		indentUnit: 4,
 		lineNumbers: true,
 		lineWrapping: true,/*
-		mode: "javascript",
 		parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "tokenizephp.js", "parsephp.js", "parsephphtmlmixed.js"],
 		stylesheet: ["<?=DIR_CM?>/ext/xmlcolors.css", "<?=DIR_CM?>/ext/jscolors.css", "<?=DIR_CM?>/ext/csscolors.css", "<?=DIR_CM?>/ext/phpcolors.css"],
 		path: "<?=DIR_CM?>/ext/",*/
@@ -108,22 +112,36 @@ $(function() {
 	});
 	editor.on("change", function() {
 		clearTimeout(pending);
-		setTimeout(update, 400);
+		setTimeout(OL.utils.setMode, 400);
 	});
-	input = document.getElementById("select");
-	function selectTheme() {
-		var theme = input.options[input.selectedIndex].innerHTML;
+	if (choice || typeof localStorage["theme"] !== 'undefined') {
+		if (typeof localStorage["theme"] !== 'undefined')
+			choice = localStorage["theme"];
+		for (var i = 0; i < input.length; i++) {
+			var option = input.options[i];
+			if (option.value == choice) {
+				option.selected = true;
+				editor.setOption("theme", choice);	
+			}
+		}
+	}
+	OL.events.selectTheme = function() {
+		var theme = input.options[input.selectedIndex].value;
+		localStorage["theme"] = theme;
+		console.debug("%cTheme:%c " + theme, "font-weight:bold", "font-weight:normal");
 		editor.setOption("theme", theme);
 	}
-	choice = document.location.search &&
-	decodeURIComponent(document.location.search.slice(1));
-	if (choice) {
-		input.value = choice;
-		editor.setOption("theme", choice);
-	} 
-	function getMode(code) {
-		var mode;
-		if (/(^\s*\(\s*function\b|alert|document|window|location|getElementsByName|getItems|open|close|write|writeln|execCommand|queryCommandEnabled|queryCommandIndeterm|queryCommandState|queryCommandSupported|queryCommandValue|clear|getSelection|captureEvents|releaseEvents|routeEvent|domain|cookie|body|head|images|embeds|plugins|links|forms|scripts|designMode|fgColor|linkColor|vlinkColor|alinkColor|bgColor|anchors|applets|getElementsByTagName|getElementsByTagNameNS|getElementsByClassName|getElementById|createElement|createElementNS|createDocumentFragment|createTextNode|createComment|createProcessingInstruction|importNode|adoptNode|createEvent|createRange|createNodeIterator|createTreeWalker|createCDATASection|createAttribute|createAttributeNS|hasFocus|releaseCapture|enableStyleSheetsForSet|elementFromPoint|caretPositionFromPoint|querySelector|querySelectorAll|getAnonymousNodes|getAnonymousElementByAttribute|getBindingParent|loadBindingDocument|createExpression|createNSResolver|evaluate|implementation|URL|documentURI|compatMode|characterSet|contentType|doctype|documentElement|inputEncoding|referrer|lastModified|readyState|title|dir|defaultView|activeElement|onreadystatechange|onmouseenter|onmouseleave|onwheel|oncopy|oncut|onpaste|onbeforescriptexecute|onafterscriptexecute|currentScript|hidden|visibilityState|styleSheets|selectedStyleSheetSet|lastStyleSheetSet|preferredStyleSheetSet|styleSheetSets|onabort|oncanplay|oncanplaythrough|onchange|onclick|oncontextmenu|ondblclick|ondrag|ondragend|ondragenter|ondragleave|ondragover|ondragstart|ondrop|ondurationchange|onemptied|onended|oninput|oninvalid|onkeydown|onkeypress|onkeyup|onloadeddata|onloadedmetadata|onloadstart|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|onpause|onplay|onplaying|onprogress|onratechange|onreset|onseeked|onseeking|onselect|onshow|onstalled|onsubmit|onsuspend|ontimeupdate|onvolumechange|onwaiting|onononononblur|onerror|onfocus|onload|onscroll|hasChildNodes|insertBefore|appendChild|replaceChild|removeChild|normalize|cloneNode|isEqualNode|compareDocumentPosition|contains|lookupPrefix|lookupNamespaceURI|isDefaultNamespace|hasAttributes|nodeType|nodeName|baseURI|ownerDocument|parentNode|parentElement|childNodes|firstChild|lastChild|previousSibling|nextSibling|nodeValue|textContent|namespaceURI|prefix|localName|addEventListener|removeEventListener|dispatchEvent)/gi.test(code) || /^\s*[;\(]/.test(code))
+	OL.utils.setMixedMode = function(enabled=true) {
+		editor.setOption("path", ((enabled) ? "<?=DIR_CM?>/ext/" : ""));
+		editor.setOption("parserfile", ((enabled) ? ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "tokenizephp.js", "parsephp.js", "parsephphtmlmixed.js"] : []));
+		editor.setOption("stylesheet", ((enabled) ? ["<?=DIR_CM?>/ext/xmlcolors.css", "<?=DIR_CM?>/ext/jscolors.css", "<?=DIR_CM?>/ext/csscolors.css", "<?=DIR_CM?>/ext/phpcolors.css"] : []));
+	}
+	OL.utils.getMode = function(code) {
+		var mode,
+			jsKeywords = /(^\s*\(\s*function\b|alert|document|window|location|getElementsByName|getItems|open|close|write|writeln|execCommand|queryCommandEnabled|queryCommandIndeterm|queryCommandState|queryCommandSupported|queryCommandValue|clear|getSelection|captureEvents|releaseEvents|routeEvent|domain|cookie|body|head|images|embeds|plugins|links|forms|scripts|designMode|fgColor|linkColor|vlinkColor|alinkColor|bgColor|anchors|applets|getElementsByTagName|getElementsByTagNameNS|getElementsByClassName|getElementById|createElement|createElementNS|createDocumentFragment|createTextNode|createComment|createProcessingInstruction|importNode|adoptNode|createEvent|createRange|createNodeIterator|createTreeWalker|createCDATASection|createAttribute|createAttributeNS|hasFocus|releaseCapture|enableStyleSheetsForSet|elementFromPoint|caretPositionFromPoint|querySelector|querySelectorAll|getAnonymousNodes|getAnonymousElementByAttribute|getBindingParent|loadBindingDocument|createExpression|createNSResolver|evaluate|implementation|URL|documentURI|compatMode|characterSet|contentType|doctype|documentElement|inputEncoding|referrer|lastModified|readyState|title|dir|defaultView|activeElement|onreadystatechange|onmouseenter|onmouseleave|onwheel|oncopy|oncut|onpaste|onbeforescriptexecute|onafterscriptexecute|currentScript|hidden|visibilityState|styleSheets|selectedStyleSheetSet|lastStyleSheetSet|preferredStyleSheetSet|styleSheetSets|onabort|oncanplay|oncanplaythrough|onchange|onclick|oncontextmenu|ondblclick|ondrag|ondragend|ondragenter|ondragleave|ondragover|ondragstart|ondrop|ondurationchange|onemptied|onended|oninput|oninvalid|onkeydown|onkeypress|onkeyup|onloadeddata|onloadedmetadata|onloadstart|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|onpause|onplay|onplaying|onprogress|onratechange|onreset|onseeked|onseeking|onselect|onshow|onstalled|onsubmit|onsuspend|ontimeupdate|onvolumechange|onwaiting|onononononblur|onerror|onfocus|onload|onscroll|hasChildNodes|insertBefore|appendChild|replaceChild|removeChild|normalize|cloneNode|isEqualNode|compareDocumentPosition|contains|lookupPrefix|lookupNamespaceURI|isDefaultNamespace|hasAttributes|nodeType|nodeName|baseURI|ownerDocument|parentNode|parentElement|childNodes|firstChild|lastChild|previousSibling|nextSibling|nodeValue|textContent|namespaceURI|prefix|localName|addEventListener|removeEventListener|dispatchEvent)/gi;
+		if (/\<.*\>/gi.test(code) && jsKeywords.test(code) && (/\<\?php/gi.test(code) || /\<\?/gi.test(code)))
+			mode = "mixed";
+		else if (jsKeywords.test(code) || /^\s*[;\(]/.test(code))
 			mode = "javascript";
 		else if (/(select|count|insert|update|delete|from)/gi.test(code))
 			mode = "sql";
@@ -132,16 +150,18 @@ $(function() {
 		/*else if ()
 			mode = "";
 		else if ()
-			mode = "";
-		else if ()
 			mode = "";*/
-		else
+		else if (typeof mode === 'undefined')
 			mode = "text/html";
 		console.debug(mode);
 		return mode;
 	}
-	function update() {
-		editor.setOption("mode", getMode(editor.getValue()));
+	OL.utils.setMode = function () {
+		var mode = OL.utils.getMode(editor.getValue());
+		if (mode == "mixed")
+			OL.utils.setMixedMode();
+		else
+			editor.setOption("mode", mode);
 	}
 	</script>
 </body>
